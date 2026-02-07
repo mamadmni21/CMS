39aa7379277653ecece2a929af829dc805c0a341
Muhamad Nur Ismanto <97931154+mamadmni21@users.noreply.github.com>
2026-02-07 18:58:12 +0700

    update files app.py and json
    update app.py with current setting and adding vercel.json for vercel use

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from app import create_app
 
+# Vercel looks for the variable 'app'
 app = create_app()
 
+# You don't technically need the __main__ block for Vercel, 
+# but keeping it allows you to still run it locally via 'python app.py'
 if __name__ == "__main__":
     app.run(debug=True)

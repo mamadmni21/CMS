5ebae3500d26f771ff5c0ac84c23f6646131cadf
Muhamad Nur Ismanto <97931154+mamadmni21@users.noreply.github.com>
2026-02-07 20:31:13 +0700

    Refactor database config to use environment variables
    Updated database configuration to use environment variables and added SSL options for Aiven.

--- config.py
+++ config.py
@@ -1,6 +1,18 @@
-class Config:
-    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mamad2024%23%40%21@localhost:3306/cms_2025'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_2tMx7wZmnAwBTYDM8zr@mysql-25d48b51-sepuh-cms-26.a.aivencloud.com:14721/defaultdb?ssl_mode=REQUIRED'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'supersecretkey'
+import os
 
+class Config:
+    # 1. Remove the ?ssl_mode=... from the end of the URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 
+        'mysql+pymysql://avnadmin:AVNS_2tMx7wZmnAwBTYDM8zr@mysql-25d48b51-sepuh-cms-26.a.aivencloud.com:14721/cms_2025')
+    
+    # 2. Add this specific dictionary for Aiven's SSL requirement
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {
+                "ca": None  # This tells PyMySQL to use SSL without a specific file
+            }
+        }
+    }
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey')

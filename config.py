ab1cfdb3605bfaef5ca8677c1f256ea310c9c905
Muhamad Nur Ismanto <97931154+mamadmni21@users.noreply.github.com>
2026-02-07 19:33:47 +0700

    add and update
    add backup files and update the requirements and config for aiven database

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mamad2024%23%40%21@localhost:3306/cms_2025'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mamad2024%23%40%21@localhost:3306/cms_2025'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_2tMx7wZmnAwBTYDM8zrYOUR_AIVEN_PASSWORD@mysql-25d48b51-sepuh-cms-26.a.aivencloud.com:14721/defaultdb?ssl-mode=REQUIRED'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
---
+++ config_backup.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mamad2024%23%40%21@localhost:3306/cms_2025'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'supersecretkey'

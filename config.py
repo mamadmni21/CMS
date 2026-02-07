a63bbb23c7090d04c2af9d90bdbb0b705ce75bd8
Muhamad Nur Ismanto <97931154+mamadmni21@users.noreply.github.com>
2026-02-07 20:27:11 +0700

    Fix SSL mode parameter in database URI
    syntax aiven - to _

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 class Config:
     #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Mamad2024%23%40%21@localhost:3306/cms_2025'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_2tMx7wZmnAwBTYDM8zr@mysql-25d48b51-sepuh-cms-26.a.aivencloud.com:14721/defaultdb?ssl-mode=REQUIRED'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://avnadmin:AVNS_2tMx7wZmnAwBTYDM8zr@mysql-25d48b51-sepuh-cms-26.a.aivencloud.com:14721/defaultdb?ssl_mode=REQUIRED'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
+
